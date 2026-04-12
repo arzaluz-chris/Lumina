a7aaee1454365eee96c027f224177b6f8fac39de
arzaluz-chris <23008381@es.uveg.edu.mx>
2026-04-11 19:14:23 -0600

    Initial Commit

---
+++ Lumina/LuminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaApp.swift
+//  Lumina
+//
+//  Created by Christian Arzaluz on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LuminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
